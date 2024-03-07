8f5b1f2efd2520987928d43d22d3b20a409bffbb
Andrew <andrewsheu0617@gmail.com>
2024-03-07 19:56:10 +1100

    CSS version0  New add JSON

---
+++ script.js
@@ -0,0 +1,70 @@
+// Scrolling Header Color Change
+window.addEventListener('scroll', function(){
+    var navbar = document.querySelector('.header');
+
+    var scrollTop = window.scrollY;
+
+    if (scrollTop < 765) {
+        navbar.classList.remove('color2', 'color3', 'color4');
+        navbar.classList.add('color1');
+    } else if (scrollTop < 2500) {
+        navbar.classList.remove('color1', 'color3', 'color4');
+        navbar.classList.add('color2');
+    } else if (scrollTop<3515) {
+        navbar.classList.remove('color1', 'color2', 'color4');
+        navbar.classList.add('color3');
+    } else {
+        navbar.classList.remove('color1', 'color2', 'color3');
+        navbar.classList.add('color4');
+    }
+
+    var links = document.querySelectorAll('.header_font_color a');
+
+    links.forEach(function(link) {
+        if (scrollTop < 765) {
+            link.style.color = '#845007';
+        } 
+        else if (scrollTop < 2500) {
+            link.style.color = 'white';
+        } else if (scrollTop < 3515) {
+            link.style.color = '#3A4660';
+        } else {
+            link.style.color = 'white';
+        }
+    });
+
+    var backToTopButton = document.getElementById('back-to-top');
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+} )
+
+document.getElementById('back-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+});
+
+
+
+// Get all links
+var links = document.querySelectorAll('.navbar a');
+
+// Add scroll event listener for each link
+links.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default anchor behavior
+
+        // Get target element id
+        var targetId = this.getAttribute('href').substring(1);
+
+        // Scroll to the target element smoothly
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
